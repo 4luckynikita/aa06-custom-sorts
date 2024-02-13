06ff6a219be9a5173b4ac4b286735597d5e9af94
ceci <ceciliajestrada@gmail.com>
2024-02-13 14:21:16 -0800

    3 and four

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,10 +1,10 @@
 function ageSort(users) {
-  // Your code here 
+  // Your code here
   return users.sort((a, b) => a.age - b.age);
 }
 
 function oddEvenSort(arr) {
-  // Your code here 
+  // Your code here
   return arr.sort((a, b) => {
     if(a%2===1 && b%2===0) return -1;
     if(a%2===0 && b%2===1) return 1;
@@ -13,15 +13,39 @@ function oddEvenSort(arr) {
 }
 
 function validAnagrams(s, t) {
-  // Your code here 
+  // Your code here
+  let first = s.split("").sort();
+  let second = t.split("").sort();
+  for(let i = 0; i < first.length; i++){
+    if(first[i] != second[i]){
+      return false;
+    }
+  }
+  return true;
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  // Your code here
+  let triple = [];
+  let double = [];
+  let single = [];
+  for(let i = 0; i < arr.length; i++){
+    if( arr[i].toString().length === 3){
+      triple.push(arr[i])
+    } else if( arr[i].toString().length === 2){
+      double.push(arr[i])
+    } else{
+      single.push(arr[i])
+    }
+  }
+  triple.sort();
+  double.sort();
+  single.sort();
+  return triple.concat(double.concat(single))
 }
 
 function frequencySort(arr) {
-  // Your code here 
+  // Your code here
 }
 
 module.exports = [
